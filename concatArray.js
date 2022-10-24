8a8de5a5a0e23912e42c78ebbe0eabee11263e8a
Korapin <korapin.6945@mail.kmutt.ac.th>
2022-10-24 12:08:48 +0700

    concat

--- concatArray.js
+++ concatArray.js
@@ -1,6 +1,27 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  //code here
+  if((array1===null || array1 === undefined) && 
+  (array2===null || array2 === undefined))
+  {
+  return undefined
 }
+  // else if(array1.length === 0 && array2.length === 0){
+  //    return []
+  // }
+
+  else if(array1 === null || array1 === undefined){
+    return array2
+  }
+
+  else if(array2 === null || array2 === undefined) {
+    return array1
+  }
+
+  else {return array1.concat(array2)
+}
+}
+
+
+
 module.exports = concatArray
